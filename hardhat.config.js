95c79e6ec07714445e61ffca15c19a46c3669464
aditiakhauri <73890308+aditiakhauri@users.noreply.github.com>
2024-02-28 00:41:04 +0530

    chainstack connection addition

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY,API_URL_1} = process.env;
 module.exports = {
   solidity: "0.8.24",
 
@@ -13,6 +13,10 @@ module.exports = {
       sepolia: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
+      },
+      chainstack:{
+        url: API_URL_1,
+        accounts:[`0x${PRIVATE_KEY}`]
       }
     }
 };
